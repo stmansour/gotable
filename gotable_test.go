81f67f25a0f14536db33533ff856dccfbd9ce590
Sudip Raval <rsudip90.dev@gmail.com>
2017-04-04 15:58:33 +0530

    ADD: benchmark with different inputs for each export type of table

--- gotable_test.go
+++ gotable_test.go
@@ -1,6 +1,7 @@
 package gotable
 
 import (
+	// "bytes"
 	"os"
 	"testing"
 )
@@ -17,9 +18,9 @@ func getTable(rows int) *Table {
 	tbl.SetSection2("Sample Table Section Two")
 
 	// add columns
-	tbl.AddColumn("Index", 35, CELLSTRING, COLJUSTIFYLEFT)
+	tbl.AddColumn("Index", 10, CELLINT, COLJUSTIFYLEFT)
 	tbl.AddColumn("Value", 10, CELLINT, COLJUSTIFYRIGHT)
-	tbl.AddColumn("Description", 80, CELLINT, COLJUSTIFYRIGHT)
+	tbl.AddColumn("Description", 80, CELLSTRING, COLJUSTIFYLEFT)
 
 	for i := 0; i < rows; i++ {
 		tbl.AddRow()
@@ -31,32 +32,47 @@ func getTable(rows int) *Table {
 	return &tbl
 }
 
-// BenchMark of one table printing
-func BenchmarkTableString(b *testing.B) {
-	// run the table string output function b.N times
-	for n := 0; n < b.N; n++ {
-		tbl := getTable(10)
-		_ = tbl.String()
-	}
-}
+// // BenchMark of one table printing
+// func BenchmarkTableString(b *testing.B) {
+// 	// run the table string output function b.N times
+// 	for n := 0; n < b.N; n++ {
+// 		temp := bytes.Buffer{}
+// 		tbl := getTable(10)
+// 		temp.WriteString(tbl.String())
+// 	}
+// }
 
 // BenchMark of one table printing for text export
 func BenchmarkTableTEXT(b *testing.B) {
-	funcname := "BenchmarkTableTEXT"
+	funcname := "BenchMarkTableTEXT"
 
-	// run the table string output function b.N times
-	for n := 0; n < b.N; n++ {
-		tbl := getTable(10)
-		csvF, err := os.Create("benchmark.txt")
-		if err != nil {
-			b.Fatalf("Error <%s>: %s", funcname, err.Error())
-		}
-		err = tbl.CSVprintTable(csvF)
-		if err != nil {
-			b.Fatalf("Error <%s>: %s", funcname, err.Error())
-		}
-		csvF.Close()
-		os.Remove(csvF.Name())
+	benchmarks := []struct {
+		name string
+		rows int
+	}{
+		{"Table-TEXT-Rows-10", 10},
+		{"Table-TEXT-Rows-100", 100},
+		{"Table-TEXT-Rows-1000", 1000},
+	}
+
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+
+			for n := 0; n < b.N; n++ {
+				tbl := getTable(bm.rows)
+				f, err := os.Create("benchmark.txt")
+				if err != nil {
+					b.Fatalf("Error <%s>: %s", funcname, err.Error())
+				}
+				err = tbl.TextprintTable(f)
+				if err != nil {
+					b.Fatalf("Error <%s>: %s", funcname, err.Error())
+				}
+				f.Close()
+				os.Remove(f.Name())
+			}
+
+		})
 	}
 }
 
@@ -64,19 +80,33 @@ func BenchmarkTableTEXT(b *testing.B) {
 func BenchmarkTableCSV(b *testing.B) {
 	funcname := "BenchmarkTableCSV"
 
-	// run the table string output function b.N times
-	for n := 0; n < b.N; n++ {
-		tbl := getTable(10)
-		csvF, err := os.Create("benchmark.csv")
-		if err != nil {
-			b.Fatalf("Error <%s>: %s", funcname, err.Error())
-		}
-		err = tbl.CSVprintTable(csvF)
-		if err != nil {
-			b.Fatalf("Error <%s>: %s", funcname, err.Error())
-		}
-		csvF.Close()
-		os.Remove(csvF.Name())
+	benchmarks := []struct {
+		name string
+		rows int
+	}{
+		{"Table-CSV-Rows-10", 10},
+		{"Table-CSV-Rows-100", 100},
+		{"Table-CSV-Rows-1000", 1000},
+	}
+
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+
+			for n := 0; n < b.N; n++ {
+				tbl := getTable(bm.rows)
+				f, err := os.Create("benchmark.csv")
+				if err != nil {
+					b.Fatalf("Error <%s>: %s", funcname, err.Error())
+				}
+				err = tbl.CSVprintTable(f)
+				if err != nil {
+					b.Fatalf("Error <%s>: %s", funcname, err.Error())
+				}
+				f.Close()
+				os.Remove(f.Name())
+			}
+
+		})
 	}
 }
 
@@ -84,19 +114,33 @@ func BenchmarkTableCSV(b *testing.B) {
 func BenchmarkTableHTML(b *testing.B) {
 	funcname := "BenchmarkTableHTML"
 
-	// run the table string output function b.N times
-	for n := 0; n < b.N; n++ {
-		tbl := getTable(10)
-		csvF, err := os.Create("benchmark.html")
-		if err != nil {
-			b.Fatalf("Error <%s>: %s", funcname, err.Error())
-		}
-		err = tbl.CSVprintTable(csvF)
-		if err != nil {
-			b.Fatalf("Error <%s>: %s", funcname, err.Error())
-		}
-		csvF.Close()
-		os.Remove(csvF.Name())
+	benchmarks := []struct {
+		name string
+		rows int
+	}{
+		{"Table-HTML-Rows-10", 10},
+		{"Table-HTML-Rows-100", 100},
+		{"Table-HTML-Rows-1000", 1000},
+	}
+
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+
+			for n := 0; n < b.N; n++ {
+				tbl := getTable(bm.rows)
+				f, err := os.Create("benchmark.html")
+				if err != nil {
+					b.Fatalf("Error <%s>: %s", funcname, err.Error())
+				}
+				err = tbl.HTMLprintTable(f)
+				if err != nil {
+					b.Fatalf("Error <%s>: %s", funcname, err.Error())
+				}
+				f.Close()
+				os.Remove(f.Name())
+			}
+
+		})
 	}
 }
 
@@ -104,18 +148,32 @@ func BenchmarkTableHTML(b *testing.B) {
 func BenchmarkTablePDF(b *testing.B) {
 	funcname := "BenchmarkTablePDF"
 
-	// run the table string output function b.N times
-	for n := 0; n < b.N; n++ {
-		tbl := getTable(10)
-		csvF, err := os.Create("benchmark.pdf")
-		if err != nil {
-			b.Fatalf("Error <%s>: %s", funcname, err.Error())
-		}
-		err = tbl.CSVprintTable(csvF)
-		if err != nil {
-			b.Fatalf("Error <%s>: %s", funcname, err.Error())
-		}
-		csvF.Close()
-		os.Remove(csvF.Name())
+	benchmarks := []struct {
+		name string
+		rows int
+	}{
+		{"Table-PDF-Rows-10", 10},
+		{"Table-PDF-Rows-100", 100},
+		{"Table-PDF-Rows-1000", 1000},
+	}
+
+	for _, bm := range benchmarks {
+		b.Run(bm.name, func(b *testing.B) {
+
+			for n := 0; n < b.N; n++ {
+				tbl := getTable(bm.rows)
+				f, err := os.Create("benchmark.pdf")
+				if err != nil {
+					b.Fatalf("Error <%s>: %s", funcname, err.Error())
+				}
+				err = tbl.PDFprintTable(f)
+				if err != nil {
+					b.Fatalf("Error <%s>: %s", funcname, err.Error())
+				}
+				f.Close()
+				os.Remove(f.Name())
+			}
+
+		})
 	}
 }
